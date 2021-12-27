6b83d0878460f3312325112207e0e4c1cf9b0cad
TheAmid <45180999+TheAmid@users.noreply.github.com>
2021-12-27 20:35:33 +0400

    renaming

--- rtspm/_spm_slice_vol.py
+++ rtspm/_spm_slice_vol.py
@@ -1,6 +1,6 @@
 import _rtspm as spm
 
 
-def spm_slice_vol(vol, img, mat_t, temp):
+def spm_slice_vol(vol, img, mat, temp):
 
-    spm.slice_vol(vol, img, mat_t, temp)
+    spm.slice_vol(vol, img, mat, temp)
